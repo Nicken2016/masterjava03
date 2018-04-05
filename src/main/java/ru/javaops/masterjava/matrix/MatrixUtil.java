cdf24a4d415403709a7e68848c3753bb27c2bdf9
John Doe <johndoe@example.com>
2018-04-05 17:32:27 +0600

    Init Masterjava

---
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -0,0 +1,54 @@
+package ru.javaops.masterjava.matrix;
+
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+
+public class MatrixUtil {
+    public static int[][] create(int size) {
+        int[][] matrix = new int[size][size];
+        Random rn = new Random();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = rn.nextInt(10);
+            }
+        }
+        return matrix;
+    }
+
+    public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
+        final int matrixSize = matrixA.length;
+        final int[][] matrixC = new int[matrixSize][matrixSize];
+
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                int sum = 0;
+                for (int k = 0; k < matrixSize; k++) {
+                    sum += matrixA[i][k] * matrixB[k][j];
+                }
+                matrixC[i][j] = sum;
+            }
+        }
+        return matrixC;
+    }
+
+    public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
+        final int matrixSize = matrixA.length;
+        final int[][] matrixC = new int[matrixSize][matrixSize];
+
+        return matrixC;
+    }
+
+    public static boolean compare(int[][] matrixA, int[][] matrixB) {
+        final int matrixSize = matrixA.length;
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                if (matrixA[i][j] != matrixB[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
