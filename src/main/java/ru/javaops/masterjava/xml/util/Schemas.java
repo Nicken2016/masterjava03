00b13447fd596e4dd9a4e3ed95eac52a7ef9480b
Nicken <onicken@yandex.ru>
2018-05-05 00:20:45 +0600

    2_07_JAXB

---
+++ src/main/java/ru/javaops/masterjava/xml/util/Schemas.java
@@ -0,0 +1,45 @@
+package ru.javaops.masterjava.xml.util;
+
+import com.google.common.io.Resources;
+import org.xml.sax.SAXException;
+
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import java.io.File;
+import java.io.StringReader;
+import java.net.URL;
+
+public class Schemas {
+    private static final SchemaFactory SCHEMA_FACTORY = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+
+    public static synchronized Schema ofString(String xsd) {
+        try {
+            return SCHEMA_FACTORY.newSchema(new StreamSource(new StringReader(xsd)));
+        } catch (SAXException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public static synchronized Schema ofClasspath(String resource) {
+        return ofURL(Resources.getResource(resource));
+    }
+
+    public static synchronized Schema ofURL(URL url) {
+        try {
+            return SCHEMA_FACTORY.newSchema(url);
+        } catch (SAXException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public static synchronized Schema ofFile(File file) {
+        try {
+            return SCHEMA_FACTORY.newSchema(file);
+        } catch (SAXException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+}
