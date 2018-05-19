d9eda9ac8c318658402f6ce3bc51f12700ef3900
Nicken <onicken@yandex.ru>
2018-05-19 18:47:41 +0600

    5_4_HW4_parallel

--- web/upload/src/main/java/ru/javaops/masterjava/upload/UploadServlet.java
+++ web/upload/src/main/java/ru/javaops/masterjava/upload/UploadServlet.java
@@ -4,7 +4,6 @@ import com.google.common.collect.ImmutableMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.thymeleaf.context.WebContext;
-import ru.javaops.masterjava.persist.model.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -20,10 +19,10 @@ import java.util.List;
 import static ru.javaops.masterjava.common.web.ThymeleafListener.engine;
 
 @WebServlet(urlPatterns = "/", loadOnStartup = 1)
-@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 10)//10 MB in memory limit
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 10) //10 MB in memory limit
 public class UploadServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(UploadServlet.class);
-    public static final int CHUNK_SIZE = 2000;
+    private static final int CHUNK_SIZE = 2000;
 
     private final UserProcessor userProcessor = new UserProcessor();
 
@@ -35,20 +34,20 @@ public class UploadServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String message;
-        int chunckSize = CHUNK_SIZE;
-
+        int chunkSize = CHUNK_SIZE;
         try {
-            chunckSize = Integer.parseInt(req.getParameter("chunkSize"));
-            if (chunckSize < 1) {
+//            http://docs.oracle.com/javaee/6/tutorial/doc/glraq.html
+            chunkSize = Integer.parseInt(req.getParameter("chunkSize"));
+            if (chunkSize < 1) {
                 message = "Chunk Size must be > 1";
             } else {
                 Part filePart = req.getPart("fileToUpload");
                 try (InputStream is = filePart.getInputStream()) {
-                    List<User> alreadyPresentUsers = userProcessor.process(is, chunckSize);
-                    log.info("Already present in DB " + alreadyPresentUsers.size() + " users");
+                    List<UserProcessor.FailedChunk> failed = userProcessor.process(is, chunkSize);
+                    log.info("Failed users: " + failed);
                     final WebContext webContext =
                             new WebContext(req, resp, req.getServletContext(), req.getLocale(),
-                                    ImmutableMap.of("users", alreadyPresentUsers));
+                                    ImmutableMap.of("users", failed));
                     engine.process("result", webContext, resp.getWriter());
                     return;
                 }
@@ -57,14 +56,13 @@ public class UploadServlet extends HttpServlet {
             log.info(e.getMessage(), e);
             message = e.toString();
         }
-        out(req, resp, message, chunckSize);
+        out(req, resp, message, chunkSize);
     }
 
-    private void out(HttpServletRequest req, HttpServletResponse resp, String message, int chunkSize) throws
-            IOException {
+    private void out(HttpServletRequest req, HttpServletResponse resp, String message, int chunkSize) throws IOException {
         resp.setCharacterEncoding("utf-8");
         final WebContext webContext = new WebContext(req, resp, req.getServletContext(), req.getLocale(),
-                ImmutableMap.of("message", message, "chunckSize", chunkSize));
+                ImmutableMap.of("message", message, "chunkSize", chunkSize));
         engine.process("upload", webContext, resp.getWriter());
     }
 }
