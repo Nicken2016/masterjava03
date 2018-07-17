b57b9c4906f182ee85c59031ff83fca97b91d09a
Nicken <onicken@yandex.ru>
2018-07-18 00:59:08 +0600

    8_5_soap_exceptions

--- services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
+++ services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
@@ -1,11 +1,13 @@
 package ru.javaops.masterjava.service.mail;
 
 import com.google.common.collect.ImmutableSet;
+import ru.javaops.web.WebStateException;
 
 public class MailWSClientMain {
-    public static void main(String[] args) {
-        MailWSClient.sendToGroup(
+    public static void main(String[] args) throws WebStateException {
+        String state = MailWSClient.sendToGroup(
                 ImmutableSet.of(new Addressee("To <nick-n71@mail.ru>")),
                 ImmutableSet.of(new Addressee("Copy <nicken@post.kz>")), "Subject", "Body");
+        System.out.println(state);
     }
 }
